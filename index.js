6fe914ed437aa2f4b126991cd64a75b86636e8db
LeightonSkervin <lskervin23@gmail.com>
2024-01-26 07:57:07 -0500

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main = document.getElementById("main")
+main.remove()
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Leighton is the champion"
+document.body.append(newHeader)
